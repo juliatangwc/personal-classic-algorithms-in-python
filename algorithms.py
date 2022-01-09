40a73b88cd326e74d365573473a58f5a19f51a76
Julia <juliatangwc@gmail.com>
2022-01-09 08:07:09 +0000

    Added Bubble Sort

---
+++ algorithms.py
@@ -0,0 +1,8 @@
+# Bubble sort
+
+def bubble_sort(list):
+    for i in range(len(list)):
+        for j in range(len(list)-1):
+            if list[j] > list[j+1]:
+                list[j], list[j+1] = list[j+1], list[j]
+    return list
